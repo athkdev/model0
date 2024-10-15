46cd002e7fda9511922e37b3725006e4b2c80f6f
AtharvaKamble <atharvadkamble00@gmail.com>
2024-10-14 20:52:04 -0400

    add: login, signup and logout with token management

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from shutil import which
 from urllib import request
 
 from django.shortcuts import render, get_object_or_404
@@ -16,45 +17,63 @@ def get_users(request):
     return Response(UserSerializer(User.objects.all(), many=True).data)
 
 @api_view(['POST'])
-def create_user(request):
-    serializer = UserSerializer(data=request.data)
+def login(request):
+    '''
+    Return the JWT token for a user after successful login and authenticate the user
+    '''
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    try:
+        user = get_object_or_404(User, username=request.data['email'])
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        password = request.data['password']
+
+        if not user.check_password(password):
+            return Response({'authenticated': 'false','error': 'AuthenticationError: Email or password does not match.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        payload = {
+            'authenticated': 'true',
+            'token': token.key,
+            'user': UserSerializer(user).data
+        }
+
+        return Response(payload, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': 'Login: user does not exist'}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
-def login(request):
+def logout(request):
+    try:
+        user_id = request.data.get('id')
+        user = get_object_or_404(User, id=user_id)
 
-    user = get_object_or_404(User, username=request.data['email'])
+        if user.is_authenticated:
+            token = Token.objects.get(user=user)
+            token.delete()
 
-    if not user.check_password(request.data['password']):
-        pass
+        return Response({'authenticated': 'false'}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': 'Logout: user does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-    serializer = UserSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-
-
-    return Response({})
 
 @api_view(['POST'])
 def signup(request):
+    '''
+    Return the JWT token for a user after successful signup and authenticate the user
+    '''
 
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
-        serializer.save()
 
-        user = User.objects.get(email=request.data['email'])
+        user = User.objects.get(email=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
 
         token = Token.objects.create(user=user)
+
+        serializer.save()
 
         return Response({'token': token.key})
 
