a7d1bff914202e0728098ea41957668d58c15419
AtharvaKamble <atharvadkamble00@gmail.com>
2025-04-20 12:36:10 -0400

    fix: add boto3 region

--- server/api/services.py
+++ server/api/services.py
@@ -1,13 +1,17 @@
 import os
+import re
 import boto3
+from botocore import regions
 
 sagemaker_client = boto3.client(
     "sagemaker",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+    region_name=os.getenv("AWS_REGION"),
 )
 runtime_client = boto3.client(
     "sagemaker-runtime",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+    region_name=os.getenv("AWS_REGION"),
 )
